514c45d5cd0c3edf8b1702ec860fe5cca2b3838b
Guillermo Niebla <nieblarobertguillermo@gmail.com>
2025-03-22 16:08:27 +0100

    mejora de la estructura HTML

--- juegodadosescalabre/registrarse.php
+++ juegodadosescalabre/registrarse.php
@@ -1,43 +1,40 @@
-<HTML>
-
-<HEAD> 
- <meta charset="UTF-8">
+<!DOCTYPE html>
+<html lang="es">
+<head> 
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <title>JUEGO DADOS - PRÁCTICA OBLIGATORIA</title>
+    <title>Juego de Dados - Práctica Obligatoria</title>
     <link rel="stylesheet" href="./bootstrap.min.css">
-  </head>
+</head>
 
-</HEAD>
+<body>
+  
 
-<BODY>
+  <form name="juegodados" action="registro.php" method="post">
 
-<form name='juegodados' action='registro.php' method='post'>
-
-<div class="container ">
-        <!--Aplicacion-->
-		<div class="card border-success mb-3" style="max-width: 30rem;">
-		<div class="card-header"><B>JUEGO DADOS</B> </div>
-		<div class="card-body">
-
-
-
- 
-<B>Nombre: </B><input type='text' name='nombre'  size=25><br><br> 
-<B>Apellido: </B><input type='text' name='apellido'  size=25><br><br> 
-<B>Correo Electrónico: </B><input type='text' name='correo'  size=25><br><br> 
+    <div class="container">
+      <!-- Aplicación -->
+      <div class="card border-success mb-3" style="max-width: 30rem;">
+        <div class="card-header">
+          <b>JUEGO DADOS</b>
+        </div>
+        <div class="card-body">
+          <b>Nombre:</b> <input type="text" name="nombre" size="25"><br><br>
+          <b>Apellido:</b> <input type="text" name="apellido" size="25"><br><br>
+          <b>Correo Electrónico:</b> <input type="text" name="correo" size="25"><br><br>
 
 
-<div>
+          <!-- boton -->
+          <div>
+            <input type="submit" value="Registrar" name="registrar" class="btn btn-warning disabled">
+          </div>
 
-	<input type="submit" value="Registrar" name="registrar" class="btn btn-warning disabled">
-		
-			
-		
-</div>	
-</FORM>
+        </div>
+      </div>        
+    </div>
+   
+  </form>
 
-
-</BODY>
-
-</HTML>
+</body>
+</html>
