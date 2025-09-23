87b8ea435dd9bc7e29db6e66b9908cd551385387
Guillermo Niebla <nieblarobertguillermo@gmail.com>
2025-09-23 12:30:54 +0200

    eje3 en proceso

--- 01_Strings/eje3s.php
+++ 01_Strings/eje3s.php
@@ -1,10 +1,11 @@
 <HTML>
-<HEAD><TITLE> EJ2-Direccion Red – Broadcast y Rango</TITLE></HEAD>
+<HEAD><TITLE> EJ3-Direccion Red – Broadcast y Rango</TITLE></HEAD>
 <BODY>
 <?php
 $ip="192.168.16.100/16";
 
-
+/**En la variable array2 separo la ip en cada casilla.
+ */
 $array1=explode("/", $ip);
 $array2=explode(".", $array1[0]);
 $ipArray=array_merge($array2, [$array1[1]]);
@@ -25,11 +26,30 @@ function convertirBinario($ip){
     }
     return $ipBinario;
 }
+echo "<br/>";
+
+//obtengo un string con la ip en binario.
+$ipBinario=convertirBinario($array1[0]);
+$ipBinArray=explode(".",$ipBinario);
+
+$mascaraBin=array();
+$cont=0;
+for ($i=0; $i < count($ipBinArray); $i++) { 
+   
+    $mascaraBin[$i]="";
+    while ($array1[1] <= $cont){
+        if($cont < $array1[1]){
+            $mascaraBin[$i].="1";
+            $cont+=1;
+        }else{
+            $mascaraBin[$i].=str_pad($mascaraBin[$i],8,0,STR_PAD_RIGHT);
+
+        }
+    }
+}
 
 
-
-
-
+print_r ($mascaraBin);
 
 ?>
 </BODY>
