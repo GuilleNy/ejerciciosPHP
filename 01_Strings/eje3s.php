0aae25fa56036c833977be55a3712b6b7d16ac84
GuilleNy <nieblarobertguillermo@gmail.com>
2025-09-27 23:56:17 +0200

    hecho hasta la direccion de red

--- 01_Strings/eje3s.php
+++ 01_Strings/eje3s.php
@@ -2,14 +2,25 @@
 <HEAD><TITLE> EJ3-Direccion Red – Broadcast y Rango</TITLE></HEAD>
 <BODY>
 <?php
-$ip="192.168.16.100/21";
+$ip="192.168.16.100/16";
 
-//print_r($ipArray);
+$num_masc=explode("/", $ip);
+$ip_dec=$num_masc[0];
 
-function convertirBinario($ip){
+$ipBinario=convertirIpBinario($ip_dec);
+$mascara=obtenerMascara($num_masc[1], $ipBinario);
+
+print_r (convertirIpBinario($ip)) ;
+echo "<br/>";
+
+print_r(obtenerMascara($num_masc[1], $ipBinario));
+
+echo "<br/>";
+echo obtenerDireccionRed($mascara, $ipBinario);
+
+function convertirIpBinario($ip){ //devuelve un array
     $ipBinario="";
-    $array1=explode("/", $ip);// obtengo en el indice 1 la cadena ip y en el indice 2 la mascara.
-    $ipArray=explode(".", $array1[0]);//obtengo en una array la ip separado
+    $ipArray=explode(".", $ip);//obtengo en una array la ip separado
 
     for ($i=0; $i < count($ipArray) ; $i++) {
         $ipBinario.=str_pad(sprintf("%b",$ipArray[$i]),8,0,STR_PAD_LEFT);
@@ -19,23 +30,17 @@ function convertirBinario($ip){
             $ipBinario.=" ";
         }
     }
-    return $ipBinario;
+    $ip_arr=explode(".", $ipBinario );
+    return $ip_arr;
 }
-echo convertirBinario($ip);
-echo "<br/>";
+
 
 //obtengo un string con la ip en binario.
-function obtenerMascara($cadenaIp){
-
-    $array1=explode("/", $cadenaIp);// obtengo en el indice 1 la cadena ip y en el indice 2 la mascara.
-    
-    $ipBinario=convertirBinario($array1[0]);//aqui obtengo la ip en binario  "11000000.10101000.00010000.01100100"
-    $ipBinArray=explode(".",$ipBinario);//separo en cada casilla los bits
-    $mascara=$array1[1];
+function obtenerMascara($masc, $ipBinario){     //devuelve un array
+    $mascara=$masc;
     $mascaraBin=array();
-    $cont=0;
 
-    for ($i=0; $i < count($ipBinArray); $i++) { 
+    for ($i=0; $i < count($ipBinario); $i++) { 
     
         if($mascara >= 8){
             $mascaraBin[$i]="11111111";
@@ -46,31 +51,43 @@ function obtenerMascara($cadenaIp){
             $mascaraBin[$i]=str_pad($mascaraBin[$i],8,0,STR_PAD_RIGHT);
         }
     }
-    $mascaraCadena=implode(".",$mascaraBin);
-    return $mascaraCadena;
+    return $mascaraBin; //devuelve una cadena 
 }
-$mascara=obtenerMascara($ip);
 
-echo obtenerMascara($ip);
 
-function convertirDecimal($ipBinArray){
-    $ipDecimal="";
-    $ipBinArray=explode(".",$ipBinArray);
 
-    for ($i=0; $i < count($ipBinArray) ; $i++) {
-        $ipDecimal.=bindec($ipBinArray[$i]);
-        if($i < count($ipBinArray) - 1){
-            $ipDecimal.=".";
+function obtenerDireccionRed($mascaraBin , $ipBin){
+    //$mascaraBin
+    //$ipBin
+    $red=array();
+
+    for ($i=0; $i < count($mascaraBin) ; $i++) { 
+        $redBinario="";
+        for ($j=0; $j < 8; $j++) { 
+            if($mascaraBin[$i][$j]== "1" && $ipBin[$i][$j] =="1"){
+                $redBinario.= "1";
+            }else{
+                $redBinario.= "0";
+            }
+        }
+        $red[]=$redBinario;
+    }
+
+    $ipDec="";
+    for ($i=0; $i < count($red) ; $i++) {
+        $ipDec.=bindec($red[$i]);
+        if($i < count($red) - 1){
+            $ipDec.=".";
         }else{
-            $ipDecimal.=" ";
+            $ipDec.=" ";
         }
     }
-    return $ipDecimal;
+    return $ipDec;
 }
 
-echo "<br/>";
-echo convertirDecimal($mascara);
 
+
+/*
 
 function obtenerbroadcast($ip){
     $ipBinario=explode(".", convertirBinario($ip)); //"11000000.10101000.00010000.01100100"
@@ -86,13 +103,7 @@ function obtenerbroadcast($ip){
     }    
 }
 
-
-if($i < count($ipArray) - 1){
-            $ipBinario.=".";
-        }else{
-            $ipBinario.=" ";
-        }
-
+*/
 
 ?>
 </BODY>
