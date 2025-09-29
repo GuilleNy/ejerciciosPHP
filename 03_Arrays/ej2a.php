a9388ceec9ec121d58c333daf06c2015935ff548
GuilleNy <nieblarobertguillermo@gmail.com>
2025-09-29 13:02:28 +0200

    ejercicio 3 array hecho

--- 03_Arrays/ej2a.php
+++ 03_Arrays/ej2a.php
@@ -8,7 +8,7 @@ echo "<table table border='1' style='border-collapse: collapse; width: 200px;'>"
 $impares=array();
 $cont=0;
 $i=1;
-while($cont <= 20){
+while($cont < 20){
     if(($i % 2) != 0){
         $impares[]=$i;
         $cont++;
@@ -22,15 +22,30 @@ echo "<tr>";
 echo "</tr>";
 
 $suma=0;
-for ($i=0; $i < count($impares); $i++) { 
-    $suma+=$impares[$i];
+$suma_pares=0;
+$suma_impares=0;
+foreach ($impares as $indice => $value) { 
+    $suma+=$value;
+
+    if($indice % 2 == 0 ){
+        $suma_pares+=$value;
+    }else{
+        $suma_impares+=$value;
+    }
+
     echo "<tr>";
-    echo "<td>" . $i .  "</td>";
-    echo "<td>" . $impares[$i] .  "</td>";
+    echo "<td>" . $indice .  "</td>";
+    echo "<td>" . $value .  "</td>";
     echo "<td>" . $suma .  "</td>";
     echo "</tr>";
 }
 echo "</table>";
+
+$media_pares=$suma_pares / (count($impares) / 2);
+$media_impares=$suma_impares / (count($impares) / 2);
+
+echo "Media pares= " . $media_pares . "</br>";
+echo "Media impares= " . $media_impares;
 ?>
 </BODY>
 </HTML>
---
+++ 03_Arrays/ej3a.php
@@ -0,0 +1,33 @@
+<HTML>
+<HEAD><TITLE> Ejercicios Arrays Unidimensionales</TITLE></HEAD>
+<BODY>
+<?php
+
+echo "<table table border='1' style='border-collapse: collapse; width: 200px;'>";
+
+echo "<tr>";
+    echo "<th>Indice</th>";
+    echo "<th>Binario</th>";
+    echo "<th>Octal</th>";
+echo "</tr>";
+
+$binario=array();
+$cont=0;
+while($cont < 20){
+    $binario[]=sprintf("%b",$cont);
+    $cont++;
+}
+
+
+foreach ($binario as $indice => $value) {
+    echo "<tr>";
+    echo "<td>" . $indice .  "</td>";
+    echo "<td>" . $value .  "</td>";
+    echo "<td>" . sprintf("%o",$indice) .  "</td>";
+    echo "</tr>";
+}
+    
+echo "</table>";
+?>
+</BODY>
+</HTML>
