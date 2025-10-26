0bf21482575b98bd8414284fbd1b0426b3bc4bb7
GuilleNy <nieblarobertguillermo@gmail.com>
2025-10-27 00:07:41 +0100

    nada

--- 06_Ficheros/bolsa/bolsa1.php
+++ 06_Ficheros/bolsa/bolsa1.php
@@ -41,13 +41,44 @@ function mostrarFichero(){
     */
 
     //vamos a utilizar el explode
-    echo $lineas[5];
+    //echo $lineas[5];
     $cadena=explode(" ",$lineas[5]);
-
-    echo "<pre>";
     print_r($cadena);
-    echo "</pre>";
+    $indice=0;
+    echo "<table table border='1' style='border-collapse: collapse; width: 200px;'>";
+    echo "<tr>";
+        echo "<td>Valor</td>";
+        echo "<td>Ultimo</td>";
+        echo "<td>Var. %</td>";
+        echo "<td>Var.</td>";
+        echo "<td>Ac.% año</td>";
+        echo "<td>MAx.</td>";
+        echo "<td>MIn.</td>";
+        echo "<td>Vol.</td>";
+        echo "<td>Capit.</td>";
+    echo "</tr>";
 
+    echo "<tr>";
+
+    $cade_2=implode("#", $cadena);
+    $cede_3=explode("##",$cade_2);
+
+    foreach ($cadena as $valor) {
+        if($valor != " "){
+            echo "<td>" . $valor . " </td>";
+        }
+        
+    }
+            
+    echo "</tr>";
+
+    echo "</table>";
+    echo $cade_2;
+    
+    echo "<pre>";
+    print_r($cede_3);
+    echo "</pre>";
+    
 }
 
 ?>
