e96feb38bfd604040e4c68a57c8030a11da0d1a5
GuilleNy <nieblarobertguillermo@gmail.com>
2025-10-27 15:06:39 +0100

    en proceso

--- DWES_Reto2_SieteMedia/media7.php
+++ DWES_Reto2_SieteMedia/media7.php
@@ -10,6 +10,10 @@ function resultado(){
         if(verificarCampos()){
             $arrayJugadores=array();
             $arrayCartas=array("1C","1D","1P","1T","2C","2D","2P","2T", "3C","3D","3P","3T", "4C","4D","4P","4T", "5C","5D","5P","5T", "6C","6D","6P","6T", "7C","7D","7P","7T", "JC","JD","JP","JT", "KC","KD","KP","KT", "QC","QD","QP","QT");
+            $arrayGanadores=array();
+
+            
+
             recogerDatos($arrayJugadores);  //Recojo los nombres de los jugadores en un array
             $num_cartas=intval(depurar($_POST['numcartas'])); //numero de cartas
             $cant_apost=intval(depurar($_POST['apuesta']));    //cantidad para apostar
@@ -19,8 +23,10 @@ function resultado(){
 
             repartirCartas($arrayJugadores, $arrayCartas, $num_cartas);
             visualizarTabla($arrayJugadores);
+            calcularPuntuacion($arrayJugadores);
+            calcularGanadores($arrayJugadores, $arrayGanadores, $arrayNoGanadores);
 
-
+            repartirPremio($arrayJugadores, $arrayGanadores);
 
 
             echo "<pre>";
@@ -28,8 +34,10 @@ function resultado(){
             echo "</pre>";
 
             echo "<pre>";
-            print_r($arrayCartas); // visualizo los jugadores
+            print_r($arrayGanadores); // visualizo los jugadores
             echo "</pre>";
+
+
             
         }
     }
--- DWES_Reto2_SieteMedia/media7fun.php
+++ DWES_Reto2_SieteMedia/media7fun.php
@@ -7,8 +7,7 @@ function depurar($cadena){
     $cadena=htmlspecialchars($cadena);
     return $cadena; 
 }
-
-
+ 
 function verificarCampos(){
 
     $mensaje="";
@@ -53,18 +52,20 @@ function recogerDatos(&$arrayJugadores){
 
     for ($i=0; $i < 4; $i++) { 
         $jugador=depurar($_POST["nombre".($i+1)]);
-        $arrayJugadores[$jugador]=array();
+        $arrayJugadores[$jugador]['cartas']=array(); //me creo un array para almacenar las cartas
+        $arrayJugadores[$jugador]['datos']=array("puntos"=>0, "premio"=>0); // me creo otro array de los datos que tendra los puntos y premio.
+
     }
 }
-
 
 function visualizarTabla($arrayJugadores){
     echo "<table border='1' style='border-collapse: collapse; width:40px;'>";
     foreach ($arrayJugadores as $nombre => $dato) {
         echo "<tr>"; 
         echo "<td > " . $nombre . "</td>";
-        foreach ($dato as $indice => $carta) {
-            echo "<td> <img src='images/" . $carta . ".PNG' ></td>";
+        foreach ($dato['cartas'] as $indice) {
+           
+            echo "<td> <img src='images/" . $indice . ".PNG'width='100' ></td>";
         }
         echo "</tr>";
     }
@@ -76,7 +77,8 @@ function repartirCartas(&$arrayJugadores, &$arrayCartas, $num_cartas){
         for ($i=0; $i < $num_cartas; $i++) { 
             $indice=array_rand($arrayCartas);
             $valor_carta=$arrayCartas[$indice];
-            $arrayJugadores[$nombre][]=$valor_carta;
+            
+            $arrayJugadores[$nombre]['cartas'][]=$valor_carta;
 
             //elimino la carta del array
             unset($arrayCartas[$indice]);
@@ -85,6 +87,51 @@ function repartirCartas(&$arrayJugadores, &$arrayCartas, $num_cartas){
     $arrayCartas = array_values($arrayCartas); //reindexo el arraycartas para que el indice se restablezca ya que al usar unset(se elimina el indice y el contenido.) 
 }
 
+function calcularPuntuacion(&$arrayJugadores){
+    $cartNum=array("1","2","3","4","5","6","7");
+    
+    foreach ($arrayJugadores as $nombre => &$dato) {
+        foreach ($dato['cartas'] as $indice => $value) {
 
+            if(in_array($value[0],$cartNum)){
+                $dato['datos']['puntos']+=intval($value[0]);
+            }else{
+                $dato['datos']['puntos']+=0.5;
+            }
+
+            //echo $value[0] . "";
+            //echo $dato['datos']['puntos'] . "\n";
+        }
+    }
+}
+
+function calcularGanadores(&$arrayJugadores, &$arrayGanadores, &$arrayNoGanadores){
+
+    foreach ($arrayJugadores as $nombre => &$dato) {
+        foreach ($dato['datos'] as $indice => $value) {
+            if($value == 7.5){
+                $arrayGanadores['ganadores'][$nombre][]=$value;
+            }elseif($value < 7.5){
+                $dato['datos']['premio']=0;
+                $arrayGanadores['puntosCercanos'][$nombre][]=$value;
+            }
+        
+            echo $value . "";
+        }
+    }
+}
+
+function repartirPremio($arrayJugadores, $arrayGanadores){
+
+    foreach ($arrayGanadores as $categ => $dato) {
+        foreach ($dato as $nombre => $value) {
+            //if($dato[$categ][$nombre])
+        
+            //echo $value . "";
+        }
+    }
+
+
+}
 
 ?>
