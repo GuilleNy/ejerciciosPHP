eb14dbcbd6f68c120dc700f5d41f032def9b295b
GuilleNy <nieblarobertguillermo@gmail.com>
2025-11-19 01:00:18 +0100

    7 hecho

--- web_compras/comconscom.php
+++ web_compras/comconscom.php
@@ -80,46 +80,83 @@ function verifica_campo(){
     $enviar = True;  
 
 
-    if(!isset($_POST['almacen'])){
-        $mensaje .= "No se ha seleccionado una Producto.";
+    if(!isset($_POST['cliente'])){
+        $mensaje .= "No se ha seleccionado un Cliente. <br>";
         $enviar = False; 
     }
+    
+
+    if (empty($_POST['fecha_desde'])) {
+        $mensaje .= "El campo fecha desde esta vacio. <br>";
+        $enviar = False;  
+    }
+
+    if (empty($_POST['fecha_hasta'])) {
+        $mensaje .= "El campo fecha hasta esta vacio. <br>";
+        $enviar = False;  
+    }
+    
     echo $mensaje;
     return $enviar;
 }
 
 function visualizarCompras(){
 
-    $numAlm = depurar($_POST['almacen']);
-    $all_stock = obtenerCompras($numAlm);
+    $nif = depurar($_POST['cliente']);
+    $fecha_desde =depurar($_POST['fecha_desde']);   
+    $fecha_hasta =depurar($_POST['fecha_hasta']);
+    $total = totalCompras($nif, $fecha_desde, $fecha_hasta);
+
+   
+    $all_productos = obtenerCompras($nif , $fecha_desde, $fecha_hasta);
+
 
     echo "<table class='table table-striped w-50 mx-auto'>";
         echo "<tr>
                 <th>Producto</th>
-                <th>Cantidad</th>
+                <th>Nombre</th>
+                <th>Precio</th>
               </tr>";
 
         // Recorrer filas
-        foreach ($all_stock as $fila) {
+        if(empty($all_productos)){
+            echo "No hay datos en esas fechas";
+            
+        }else{
+            foreach ($all_productos as $fila) {
+                echo "<tr>";
+                echo "<td>" . $fila['categoria'] . "</td>";
+                echo "<td>" . $fila['producto'] . "</td>";
+                echo "<td>" . $fila['precio'] . "</td>";
+                echo "</tr>";
+            }
             echo "<tr>";
-            echo "<td>" . $fila['NOMBRE'] . "</td>";
-            echo "<td>" . $fila['CANTIDAD'] . "</td>";
+                echo "<th></th>";
+                echo "<th>TOTAL</th>";
+                echo "<td>" . $total . "</td>";
             echo "</tr>";
+            
         }
+        
 
     echo "</table>";
 }
 
 
 
-function obtenerCompras($numAlm){
+function obtenerCompras($nif, $fecha_desde, $fecha_hasta){
     $conn = conexion_BBDD();
     try{    
-        $stmt = $conn->prepare("SELECT NOMBRE , CANTIDAD  
-                                FROM producto, almacena
-                                WHERE almacena.id_producto=producto.id_producto
-                                AND  num_almacen = :numAlm");
-        $stmt->bindParam(':numAlm', $numAlm);
+        $stmt = $conn->prepare("SELECT categoria.NOMBRE 'categoria', producto.NOMBRE 'producto', producto.PRECIO 'precio'
+                                FROM cliente , compra , producto , categoria
+                                WHERE cliente.NIF=compra.NIF
+                                AND compra.id_producto=producto.id_producto
+                                AND producto.id_categoria=categoria.id_categoria
+                                AND  compra.NIF = :nif_cliente
+                                AND fecha_compra BETWEEN :fecha_desde AND :fecha_hasta");
+        $stmt->bindParam(':nif_cliente', $nif);
+        $stmt->bindParam(':fecha_desde', $fecha_desde);
+        $stmt->bindParam(':fecha_hasta', $fecha_hasta);
         $stmt->execute();
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
         $all_productos=$stmt->fetchAll();
@@ -131,5 +168,27 @@ function obtenerCompras($numAlm){
     return $all_productos;
 }
 
+function totalCompras($nif, $fecha_desde, $fecha_hasta){
+
+    $conn = conexion_BBDD();
+    try{    
+        $stmt = $conn->prepare("SELECT sum(producto.precio*compra.unidades) 'total' 
+                                FROM producto , compra
+                                WHERE compra.id_producto=producto.id_producto 
+                                AND fecha_compra BETWEEN :fecha_desde AND :fecha_hasta
+                                AND compra.nif = :nif_cliente");
+        $stmt->bindParam(':nif_cliente', $nif);
+        $stmt->bindParam(':fecha_desde', $fecha_desde);
+        $stmt->bindParam(':fecha_hasta', $fecha_hasta);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $total=$stmt->fetch();
+    }catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+
+    return $total['total'];
+}
 
 ?>
